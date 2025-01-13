f1d425ecf461bc9fc9af6491585c73b41d4dfd01
Roland M <rolandmackintosh1@gmail.com>
2025-01-12 22:35:19 -0800

    partial success running MouseTracker independently

--- surveillance/src/facade/mouse_facade.py
+++ surveillance/src/facade/mouse_facade.py
@@ -1,5 +1,9 @@
-from Xlib import display
-from win32api import GetCursorPos
+from ..util.detect_os import get_os_info
+os_type = get_os_info()
+if os_type.startswith("Windows"):
+    from win32api import GetCursorPos
+if os_type.startswith("Ubuntu") or os_type.startswith("Linux"):
+    from Xlib import display
 
 class MouseApiFacade:
     def __init__(self):
