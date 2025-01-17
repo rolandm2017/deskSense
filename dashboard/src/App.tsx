2792b6b87b61dca0c5f1292d2545f31f8378ed71
Roland M <rolandmackintosh1@gmail.com>
2025-01-17 13:51:04 -0800

    start on getting data, but the backend has a problem

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,16 +1,56 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
-import LineChart from "./components/charts/LineChart"
-import BarChart from "./components/charts/BarChart"
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import LineChart from "./components/charts/LineChart";
+import BarChart from "./components/charts/BarChart";
+
+import {
+    getKeyboardReport,
+    getMouseReport,
+    getProgramReport,
+    KeyboardReport,
+    MouseReport,
+    ProgramActivityReport,
+} from "./api/getData.api";
 
 function App() {
+    const [keyboardReport, setKeyboardReport] = useState<KeyboardReport|null>(null) // prettier-ignore
+    const [mouseReport, setMouseReport] = useState<MouseReport|null>(null) // prettier-ignore
+    const [programReport, setProgramReport] = useState<ProgramActivityReport|null>(null); // prettier-ignore
+
+    useEffect(() => {
+        if (keyboardReport === null) {
+            getKeyboardReport().then((report) => {
+                setKeyboardReport(report);
+                console.log(report.count);
+            });
+        }
+    }, [keyboardReport]);
+
+    useEffect(() => {
+        if (mouseReport === null) {
+            getMouseReport().then((report) => {
+                setMouseReport(report);
+                console.log(report.count);
+            });
+        }
+    }, [mouseReport]);
+
+    useEffect(() => {
+        if (programReport === null) {
+            getProgramReport().then((report) => {
+                setProgramReport(report);
+                console.log(report.count);
+            });
+        }
+    }, [programReport]);
+
     return (
         <>
             <div>
                 <div>
-                    <h1>header</h1>
+                    <h1>DeskSense Dashboard</h1>
                 </div>
                 <div>
                     <h2>Programs</h2>
@@ -22,7 +62,7 @@ function App() {
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
