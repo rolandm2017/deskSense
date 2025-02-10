eeef1521a270837ab64b646a3d0071a1da0fc8ff
Roland M <rolandmackintosh1@gmail.com>
2025-02-10 11:09:47 -0800

    write start of precomputation dao methods; unfinished

--- surveillance/src/db/dao/timeline_entry_dao.py
+++ surveillance/src/db/dao/timeline_entry_dao.py
@@ -2,12 +2,12 @@
 from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 
 from typing import List
 
 from .base_dao import BaseQueueingDao
-from ..models import TimelineEntryObj
+from ..models import TimelineEntryObj, PrecomputedTimelineEntry
 from ...object.dto import ProgramDto
 from ...object.classes import KeyboardAggregate, MouseMoveWindow
 from ...object.enums import ChartEventType
@@ -40,6 +40,10 @@ class TimelineEntryDao(BaseQueueingDao):
     async def create(self, new_row: TimelineEntryObj):
         await self.queue_item(new_row, TimelineEntryObj)
 
+    async def create_precomputed_day(self):
+        # TODO -- stopping due to finger injury
+        pass
+
     async def read_highest_id(self):
         """Read the highest ID currently in the table"""
         async with self.session_maker() as session:
@@ -47,6 +51,20 @@ class TimelineEntryDao(BaseQueueingDao):
             result = await session.execute(query)
             max_id = result.scalar()
             return max_id or 0  # Return 0 if table is empty
+
+    async def read_entry_for_day(self, day: datetime):
+        # Get start of day (midnight) # time.min is 00:00:00
+        start_of_day = datetime.combine(day.date(), time.min)
+
+        # Get end of day (just before midnight) # time.max is 23:59:59.999999
+        end_of_day = datetime.combine(day.date(), time.max)
+        query = select(PrecomputedTimelineEntry).where(
+            PrecomputedTimelineEntry.start >= start_of_day,
+            PrecomputedTimelineEntry.end <= end_of_day
+        )
+        async with self.session_maker() as session:
+            result = await session.execute(query)
+            return result.scalars().all()
 
     async def read_day(self, day: datetime, event_type: ChartEventType) -> List[TimelineEntryObj]:
         """Read all entries for the given day"""
