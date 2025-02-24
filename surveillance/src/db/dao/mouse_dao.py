4258e73b6ab8c9cd940c15147052b677f51e1c72
Roland M <rolandmackintosh1@gmail.com>
2025-02-24 10:41:50 -0800

    add timezones to sqlalchemy models; add logging to catch alt tab window bug

--- surveillance/src/db/dao/mouse_dao.py
+++ surveillance/src/db/dao/mouse_dao.py
@@ -24,18 +24,16 @@ class MouseDao(BaseQueueingDao):
 
     async def create_from_start_end_times(self, start_time: datetime, end_time: datetime):
         mouse_move = MouseMove(start_time=start_time, end_time=end_time)
-
         if isinstance(mouse_move, MouseMoveWindow):
             raise ValueError("mouse move window found!")
-        # FIXME: A "MouseMove" goes in, but the Queue receives a MouseMoveWindow!
         await self.queue_item(mouse_move, MouseMove)
 
-    async def create_from_window(self, window: MouseMoveWindow):  # TODO: Remove
+    async def create_from_window(self, window: MouseMoveWindow):
         # Create dict first, to avoid MouseMoveWindow "infesting" a MouseMove object.
         # See SHA 52d3c13c3150c5859243b909d47d609f5b2b8600 to experience the issue.
+        self.logger.log_green("[LOG] Mouse move")
         mouse_move = MouseMove(
             start_time=window.start_time, end_time=window.end_time)
-        # FIXME: A "MouseMove" goes in, but the Queue receives a MouseMoveWindow!
         await self.queue_item(mouse_move, MouseMove)
 
     async def create_without_queue(self, start_time: datetime, end_time: datetime):
@@ -45,7 +43,7 @@ class MouseDao(BaseQueueingDao):
             end_time=end_time
         )
 
-        self.db.add(new_mouse_move)   # FIXME: this won't work w/ sessions
+        self.db.add(new_mouse_move)
         await self.db.commit()
         await self.db.refresh(new_mouse_move)
         return new_mouse_move
