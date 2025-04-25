3e5d7c314f256f4973fc8465f47419b951a2f99f
Roland M <rolandmackintosh1@gmail.com>
2025-04-25 10:27:42 -0700

    use pynput for linux alt q cmd

--- surveillance/surveillance/peripherals.py
+++ surveillance/surveillance/peripherals.py
@@ -19,6 +19,10 @@ load_dotenv()
 # Import project modules
 
 
+def on_hotkey():
+    exit_all_monitors()
+
+
 def debug_logger_aggregate(agg):
     """Debug logger that just prints the aggregate without making network requests"""
     print(f"DEBUG - Mouse aggregate: {agg}")
@@ -42,8 +46,9 @@ def exit_all_monitors():
 
 if __name__ == "__main__":
     # Detect operating system
+    print("HERE 49ru")
     current_os = OperatingSystemInfo()
-
+    print(current_os, "50ru")
     # Import the appropriate modules based on OS
     if current_os.is_windows:
         from surveillance.src.trackers.peripherals.windows.win_keyboard_detector import win_monitor_keyboard
@@ -57,18 +62,28 @@ if __name__ == "__main__":
         chosen_mouse_monitor = win_monitor_mouse
     else:
         from surveillance.src.trackers.peripherals.linux.linux_peripheral_detector import linux_monitor_keyboard, linux_monitor_mouse
-
+        print("64ru")
         keyboard_path = os.getenv("UBUNTU_KEYBOARD_PATH")
         mouse_path = os.getenv("UBUNTU_MOUSE_PATH")
 
         chosen_keyboard_monitor = linux_monitor_keyboard
         chosen_mouse_monitor = linux_monitor_mouse
-
+        print(chosen_keyboard_monitor)
+        print(chosen_mouse_monitor, "72ru")
+    print(keyboard_path, mouse_path, "73ru")
     if keyboard_path is None or mouse_path is None:
         raise ValueError("Failed to load peripheral paths")
 
     # Register global exit hotkey
-    keyboard.add_hotkey('alt+q', exit_all_monitors)
+    print("Adding exit hotkeys")
+    if current_os.is_windows:
+        keyboard.add_hotkey('alt+q', exit_all_monitors)
+    else:
+        from pynput import keyboard as linux_keyboard
+        # Create a listener that triggers on alt+q
+        with linux_keyboard.GlobalHotKeys({'<alt>+q': on_hotkey}) as h:
+            h.join()  # Keep the listener running
+
     print("Starting monitors. Press Alt+Q to exit all monitors.")
 
     # Create threads with shared running flag
