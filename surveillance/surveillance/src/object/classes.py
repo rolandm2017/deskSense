53f8ba17dc3ddabe0b2cb6543ebda02ea9ab99cc
Roland Mackintosh <rolandmackintosh1@gmail.com>
2025-04-26 20:25:54 -0700

    remove optional field in sessions thru inheritance

--- surveillance/surveillance/src/object/classes.py
+++ surveillance/surveillance/src/object/classes.py
@@ -13,46 +13,21 @@ class ProgramSessionDict(TypedDict):
     exe_path: str
     window_title: str
 
-
-class CompletedProgramSession:
-    exe_path: str
-    process_name: str
-    window_title: str
-    detail: str
-    start_time: UserLocalTime
-    end_time: UserLocalTime
-    duration: timedelta
-    productive: bool
-
-    # TODO: Transfer whole codebase to use 2-3 vers of the program session.
-
-
 class ProgramSession:
     exe_path: str
     process_name: str
     window_title: str
     detail: str
     start_time: UserLocalTime
-    end_time: Optional[UserLocalTime]
-    duration: Optional[timedelta]
     productive: bool
 
-    def __init__(self, exe_path="", process_name="", window_title="", detail="", start_time=UserLocalTime(datetime(2000, 1, 1, tzinfo=timezone.utc)), end_time=None, productive=False, duration_for_tests=None):
-        """Only use duration in testing. Don't use it otherwise. 'duration_for_tests' exists only for e2e tests thresholds"""
+    def __init__(self, exe_path="", process_name="", window_title="", detail="", start_time=UserLocalTime(datetime(2000, 1, 1, tzinfo=timezone.utc)), productive=False):
         # IF you remove the default args for this class, then you will have to do A LOT of cleanup in the test data.
         self.exe_path = exe_path
         self.process_name = process_name
         self.window_title = window_title
         self.detail = detail
         self.start_time = start_time
-        self.end_time = end_time
-        if start_time and end_time:
-            self.duration = end_time - start_time
-        else:
-            if duration_for_tests:
-                self.duration = duration_for_tests
-            else:
-                self.duration = None
         self.productive = productive
 
     def parse_time_string(self, time_str):
@@ -71,38 +46,83 @@ class ProgramSession:
         )
 
     def __str__(self):
-        end_time = self.end_time  # Was "TBD" (to be determined)
-        return f"ProgramSession(exe_path='{self.exe_path}', process_name='{self.process_name}', \n\ttitle='{self.window_title}', detail='{self.detail}', \n\tstart_time='{self.start_time}', \n\tend_time='{end_time}', duration='{self.duration}', productive='{self.productive}')"
+        return f"ProgramSession(exe_path='{self.exe_path}', process_name='{self.process_name}', \n\ttitle='{self.window_title}', detail='{self.detail}', \n\tstart_time='{self.start_time}', \n\tproductive='{self.productive}')"
 
 
-class CompletedChromeSession:
-    domain: str
-    detail: str
-    start_time: UserLocalTime
+class CompletedProgramSession(ProgramSession):
     end_time: UserLocalTime
     duration: timedelta
-    productive: bool
 
-# TODO: Convert to use CompletedChromeSession to avoid that gross "start_time is not None" bs
+    def __init__(
+        self, 
+        exe_path="", 
+        process_name="", 
+        window_title="", 
+        detail="", 
+        start_time=UserLocalTime(datetime(2000, 1, 1, tzinfo=timezone.utc)), 
+        end_time=UserLocalTime(datetime(2000, 1, 1, tzinfo=timezone.utc)), 
+        productive=False, 
+        duration_for_tests=None
+    ):
+        """Only use duration arg in testing. Don't use it otherwise. 'duration_for_tests' exists only for e2e tests thresholds"""
+        
+        # Initialize the base class first
+        super().__init__(
+            exe_path=exe_path,
+            process_name=process_name,
+            window_title=window_title,
+            detail=detail,
+            start_time=start_time,
+            productive=productive
+        )
+        
+        # Add the fields specific to CompletedProgramSession
+        self.end_time = end_time
+        
+        # Calculate duration
+        if start_time and end_time:
+            self.duration = end_time - start_time
+        elif duration_for_tests:
+            self.duration = duration_for_tests
+        else:
+            self.duration = timedelta(seconds=0)
+
+    @classmethod
+    def from_program_session(cls, session, end_time, duration_for_tests=None):
+        """
+        Create a CompletedProgramSession from an existing ProgramSession.
+        This factory method makes it easy to convert a running session to a completed one.
+        """
+        return cls(
+            exe_path=session.exe_path,
+            process_name=session.process_name,
+            window_title=session.window_title,
+            detail=session.detail,
+            start_time=session.start_time,
+            end_time=end_time,
+            productive=session.productive,
+            duration_for_tests=duration_for_tests
+        )
+    
+    def __str__(self):
+        return (f"CompletedProgramSession(exe_path='{self.exe_path}', process_name='{self.process_name}', \n\t"
+                f"title='{self.window_title}', detail='{self.detail}', \n\t"
+                f"start_time='{self.start_time}', \n\t"
+                f"end_time='{self.end_time}', duration='{self.duration}', productive='{self.productive}')")
+
+    # TODO: Transfer whole codebase to use 2-3 vers of the program session.
 
 
 class ChromeSession:
     domain: str
     detail: str
-    start_time: Optional[UserLocalTime]
-    end_time: Optional[UserLocalTime]
-    duration: Optional[timedelta]
+    start_time: UserLocalTime
     productive: bool
 
-    def __init__(self, domain, detail, start_time, end_time=None, productive=False, duration_for_tests=None):
+    def __init__(self, domain, detail, start_time, productive=False):
         self.domain = domain
         self.detail = detail
         self.start_time = start_time
-        self.end_time = end_time
-        if duration_for_tests:
-            self.duration = duration_for_tests
-        else:
-            self.duration = None
         self.productive = productive
 
     @staticmethod
@@ -122,9 +142,53 @@ class ChromeSession:
         )
 
     def __str__(self):
-        end_time = self.end_time
-        return f"ChromeSession(domain='{self.domain}', detail='{self.detail}', \n\tstart_time='{self.start_time}', \n\tend_time='{end_time}', duration='{self.duration}', productive='{self.productive}')"
+        return f"ChromeSession(domain='{self.domain}', detail='{self.detail}', \n\tstart_time='{self.start_time}', \n\tproductive='{self.productive}')"
 
+
+class CompletedChromeSession(ChromeSession):
+    end_time: UserLocalTime
+    duration: timedelta
+
+    def __init__(self, domain, detail, start_time, end_time, productive=False, duration_for_tests=None):
+        # Initialize the base class first
+        """Only use duration arg in testing. Don't use it otherwise. 'duration_for_tests' exists only for e2e tests thresholds"""
+        super().__init__(
+            domain=domain,
+            detail=detail,
+            start_time=start_time,
+            productive=productive
+        )
+        
+        # Add the fields specific to CompletedChromeSession
+        self.end_time = end_time
+        
+        # Calculate duration
+        if start_time and end_time:
+            self.duration = end_time - start_time
+        elif duration_for_tests:
+            self.duration = duration_for_tests
+        else:
+            self.duration = timedelta(seconds=0)
+
+    @classmethod
+    def from_chrome_session(cls, session, end_time, duration_for_tests=None):
+        """
+        Create a CompletedChromeSession from an existing ChromeSession.
+        This factory method makes it easy to convert a running session to a completed one.
+        """
+        return cls(
+            domain=session.domain,
+            detail=session.detail,
+            start_time=session.start_time,
+            end_time=end_time,
+            productive=session.productive,
+            duration_for_tests=duration_for_tests
+        )
+
+    def __str__(self):
+        return f"CompletedChromeSession(domain='{self.domain}', detail='{self.detail}', \n\tstart_time='{self.start_time}', \n\tend_time='{self.end_time}', duration='{self.duration}', \n\tproductive='{self.productive}')"
+
+# TODO: Convert to use CompletedChromeSession to avoid that gross "start_time is not None" bs
 
 class TabChangeEventWithLtz:
     tab_title: str
