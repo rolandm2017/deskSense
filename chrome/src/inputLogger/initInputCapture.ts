abc70c9e36fb475041da06916afc3415b0bb6b8f
Roland M <rolandmackintosh1@gmail.com>
2025-05-24 21:16:26 -0700

    clean dead code

--- chrome/src/inputLogger/initInputCapture.ts
+++
@@ -1,8 +0,0 @@
-import { initializedServerApi } from "../api";
-import { InputCaptureManager } from "./inputCaptureManager";
-import { systemInputCapture } from "./systemInputLogger";
-
-export const captureManager = new InputCaptureManager(
-    systemInputCapture,
-    initializedServerApi
-);
