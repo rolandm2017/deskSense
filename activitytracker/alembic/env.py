0efd55ea87e82f6f2bfcab168fe8f1c222afe287
Roland Mackintosh <rolandmackintosh1@gmail.com>
2026-04-07 05:55:35 -0700

    replace old broken migration chain with new starting init

--- activitytracker/alembic/env.py
+++ activitytracker/alembic/env.py
@@ -7,6 +7,8 @@ from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 
+from activitytracker.db.models import Base
+
 load_dotenv()
 
 
@@ -32,7 +34,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
