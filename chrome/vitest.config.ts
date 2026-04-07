ffdf75b2bfc30e4ce24c9ea6c2bc9fd26d90b88b
Roland Mackintosh <rolandmackintosh1@gmail.com>
2026-04-07 11:02:22 -0700

    uninstall happy-dom for security purposes

--- chrome/vitest.config.ts
+++ chrome/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        environment: "happy-dom",
+        environment: "playwright",
         include: [
             "src/**/*.{test,spec}.{js,ts}",
             "tests/**/*.{test,spec}.{js,ts}",
